3edc2fee26f81c3d5fada9b3349c508c6066367b
JoaoGDFaria <joaofaria13@gmail.com>
2024-02-28 23:16:02 +0000

    Error fix

--- TP3/script.py
+++ TP3/script.py
@@ -2,14 +2,15 @@ import re
 import sys
 
 def func(closedfile):
-    with open(closefile, "r") as openfile:
-        comportamento = False
+    with open(closedfile, "r") as openfile:
+        comportamento = True
         soma = 0
         i = 1
         text = openfile.read()
-        text.lower()
+        text = text.lower()
         entries = re.findall(r'(on|off|=|[-+]?[0-9]+)', text, flags=re.I)
         for entry in entries:
+            print(entry)
 
             if entry == "on":
                 comportamento = True
