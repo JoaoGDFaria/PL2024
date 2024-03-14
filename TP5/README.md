076c40f780bf698cf026706d0c580271bfec5925
JoaoGDFaria <joaofaria13@gmail.com>
2024-03-14 19:54:39 +0000

    feito

--- TP5/README.md
+++ TP5/README.md
@@ -1,1 +1,60 @@
-# PL2024
+# TP5
+
+## Enunciado Vending Machine
+
+Construir um programa que simule uma máquina de vending e que aceite os comandos de listar produto, depositar moedas, selecionar produtos e sair.
+Desenvolvi com e sem analisador léxico.
+
+## Ficheiro `vending_machine.py`
+
+O ficheiro `vending_machine.py` implementa uma máquina de venda automática simulada. Este script oferece funcionalidades básicas para listar produtos, selecionar itens, inserir moedas e verificar o saldo.
+
+### **Funcionalidades**
+
+- Listar Produtos:
+
+Comando: `LISTAR`
+
+Este comando exibe uma lista de produtos disponíveis, juntamente com suas quantidades e preços.
+
+- Selecionar Produto
+
+Comando: `SELECIONAR <código>`
+
+Este comando permite selecionar um produto com base no seu código. Ele verifica se o produto está disponível e se há saldo suficiente para a compra.
+
+- Inserir Moeda
+
+Comando: `MOEDA <moeda1> <moeda2> ...`
+
+Este comando permite inserir moedas na máquina. Aceita moedas de 1c, 2c, 5c, 10c, 20c, 50c, 1€ e 2€.
+
+- Sair
+
+Comando: `SAIR`
+
+Este comando finaliza a sessão na máquina de venda automática, exibindo o saldo final.
+
+
+
+## Ficheiro `lex_vending_machine.py`
+
+### **Estrutura do Código**
+
+1. **Definição de Tokens**: Definição dos tokens utilizados pelo analisador léxico.
+2. **Expressões Regulares para Tokens**: Expressões regulares para cada token definido.
+3. **Funções para Lidar com Tokens**: Funções para processar cada tipo de token, como adicionar moedas, listar produtos, selecionar um produto e sair da máquina.
+4. **Função para Calcular Troco**: Função para calcular o troco com base no saldo atual, em caso de saída.
+5. **Inicialização do Analisador Léxico**: Inicialização do analisador léxico fornecido pela biblioteca `ply`.
+6. **Função Principal**: Função principal do programa que lê entrada do usuário e processa os tokens.
+
+### **Principais Funcionalidades**
+- **Adicionar Moedas**: O token `MOEDA` é usado para adicionar moedas à máquina, atualizando o saldo e exibindo o saldo atualizado.
+- **Listar Produtos**: O token `LISTAR` permite listar os produtos disponíveis na máquina, exibindo seu código, nome, quantidade e preço.
+- **Selecionar Produto**: O token `SELECIONAR` é usado para selecionar um produto com base em seu código, deduzindo o preço do saldo e atualizando o stock do produto no ficheiro JSON.
+- **Calcular Troco**: A função `calcular_troco()` é chamada quando o usuário decide sair da máquina com saldo, calculando e exibindo o troco apropriado.
+- **Ficheiro JSON**: O código faz uso de um arquivo JSON (`stock.json`) para armazenar e recuperar informações sobre o stock de produtos.
+
+## Conclusão
+O código `lex_vending_machine.py` implementa uma máquina de venda automática simulada com funcionalidades básicas de adicionar moedas, listar produtos, selecionar produtos e calcular troco. Ele demonstra o uso do analisador léxico fornecido pela biblioteca `ply` para processar tokens de entrada de usuário em uma máquina de estados finitos.
+
--- TP5/lex_vending_machine.py
+++ TP5/lex_vending_machine.py
@@ -1,9 +1,11 @@
 import json
 import ply.lex as lex
+import re
 
 saldo = 0
+stock = []
 
-#VENDING MACHINE LEXER
+# Definindo os tokens
 tokens = (
     'LISTAR',
     'MOEDA',
@@ -11,60 +13,121 @@ tokens = (
     'SAIR'
 )
 
-t_LISTAR = r'LISTAR'
-t_SELECIONAR = r'SELECIONAR'
-t_SAIR = r'SAIR'
+# Ignorando caracteres de espaço em branco
+t_ignore = ' \t\n'
 
-t_ignore = " \t"
+# Expressões regulares para os tokens
+def t_MOEDA(t):
+    r'MOEDA\s+(1[ce]|2[ce]|5c|10c|20c|50c)'
+    global saldo
 
+    valor_moeda = t.value.split()[1]
+    if valor_moeda == "1e":
+        saldo += 100
+    elif valor_moeda == "2e":
+        saldo += 200
+    elif valor_moeda == "5c":
+        saldo += 5
+    elif valor_moeda == "10c":
+        saldo += 10
+    elif valor_moeda == "20c":
+        saldo += 20
+    elif valor_moeda == "50c":
+        saldo += 50
+    print(f"maq: Saldo = {saldo//100}e {saldo%100}c")
+
+# Função para lidar com erros de token
+def t_error(t):
+    t.lexer.skip(1)
+
+# Expressão regular para a regra 'SAIR'
+def t_SAIR(t):
+    r'SAIR'
+    global saldo
+    if saldo == 0:
+        print("maq: Sem troco, saindo.")
+        exit()
+    else:
+        resp = calcular_troco()
+        print(f"maq: Pode retirar o troco: {resp}")
+        print("maq: Até à próxima.")
+        exit()
+
+# Expressão regular para a regra 'LISTAR'
+def t_LISTAR(t):
+    r'LISTAR'
+    global stock
+    print("cod |       nome       | quantidade | preço")
+    print("--- |------------------|------------|------")
+    for produto in stock:
+        if produto['quant'] > 0:
+            print(f"{produto['cod']} | {produto['nome']:16} | {produto['quant']:10} | {produto['preco']:5}")
+
+# Expressão regular para a regra 'SELECIONAR'
+def t_SELECIONAR(t):
+    r'SELECIONAR\s[A-Z]\d{2}'
+    global saldo
+    ide = t.value.split()[1]
+    for elem in stock:
+        if elem['cod'] == ide:
+            if elem['quant'] > 0:
+                if saldo >= elem['preco']*100:
+                    print(f"Produto selecionado: {elem['nome']}")
+                    elem['quant'] -= 1
+                    with open("stock.json", "w") as f:
+                        json.dump(stock, f, indent=4)
+                    saldo -= elem['preco']*100
+
+                    print(f"Produto entregue: {elem['nome']}")
+                    print(f"maq: Saldo = {saldo//100}e {saldo%100}c")
+
+                else:
+                    print(f"Saldo insuficiente: {saldo//100}e {saldo%100}c para valor do produto: {elem['preco']}e")
+
+            else:
+                print(f"Produto esgotado: {elem['nome']}")
+            break
+    else:
+        print(f"Produto inexistente: {ide}")
+    return t
+
+# Função para calcular o troco
 def calcular_troco():
     global saldo
-   
-    # Dicionário com os valores das moedas em centavos e euros
     moedas = {
         200: '2e', 100: '1e', 50: '50c', 20: '20c', 10: '10c',
         5: '5c', 2: '2c', 1: '1c'
     }
     
     troco = {}
-    
-    for valor_moeda in sorted(moedas.keys(), reverse=True):
-        if saldo >= valor_moeda:
-            quantidade = saldo // valor_moeda
-            troco[moedas[valor_moeda]] = quantidade
-            saldo -= quantidade * valor_moeda
-    
+    if saldo > 0:
+        for valor_moeda in sorted(moedas.keys(), reverse=True):
+            if saldo >= valor_moeda:
+                quantidade = saldo // valor_moeda
+                troco[moedas[valor_moeda]] = quantidade
+                saldo -= quantidade * valor_moeda
+
     return troco
 
-
-
-def t_error(t):
-    print(f"Caracter inválido: {t.value[0]}")
-    t.lexer.skip(1)
-
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
-
-def t_MOEDA(t):
-    #MOEDA 5c, 10c, 25c, 50c, 1e, 2e
-    r"MOEDA (5c|10c|25c|50c|1e|2e)+"
-
-def t_SAIR(t):
-    global saldo
-    r"SAIR"
-    if saldo == 0:
-        print("maq: Sem troco, saíndo.")
-        exit()
-    else:
-        resp = calcular_troco()
-
-
+# Inicializando o analisador léxico
 lexer = lex.lex()
 
+# Função principal
 def main():
-    json_file = open("stock.json", "r")
-    stock = json.load(json_file)
+    global stock
+    global saldo
 
+    with open('stock.json', 'r') as f:
+        stock = json.load(f)
+    
     while True:
+        try:
+            s = input()
+        except EOFError:
+            break
+        lexer.input(s)
+        for _ in lexer:
+            pass
 
+if __name__ == "__main__":
+    main()
